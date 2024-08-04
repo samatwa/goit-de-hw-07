660450698aada47649f09621736203ee6ea8c368
OleksiyF <38247546+OleksiyF@users.noreply.github.com>
2024-08-04 15:36:57 +0300

    Update oleksiy_folder.py

--- oleksiy/oleksiy_folder.py
+++ oleksiy/oleksiy_folder.py
@@ -10,7 +10,7 @@ default_args = {
 }
 
 # Instantiate your DAG
-dag = DAG ('oleksiy_sample_dag', default_args=default_args, schedule_interval=None)
+dag = DAG ('oleksiy_folder_dag', default_args=default_args, schedule_interval=None, tags=["oleksiy"])
 
 # Define tasks
 def task1():
