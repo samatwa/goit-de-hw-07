2ab5bb6d75f2076270e374302d5396f64551ca22
OleksiyF <38247546+OleksiyF@users.noreply.github.com>
2024-08-16 19:46:01 +0300

    Update oleksiy_mysql.py

--- oleksiy/oleksiy_mysql.py
+++ oleksiy/oleksiy_mysql.py
@@ -22,38 +22,62 @@ with DAG(
 ) as dag:
 
     # Завдання для створення таблиці, якщо вона не існує
-    create_table = MySqlOperator(
-        task_id='create_table',
+    create_schema = MySqlOperator(
+        task_id='create_schema',
         mysql_conn_id=connection_name,
         sql="""
-        SELECT * FROM olympic_dataset.athlete_bio LIMIT 1;
+        CREATE DATABASE IF NOT EXISTS oleksiy;
         """
     )
 
     # Завдання для вставки даних в таблицю
-    insert_data = MySqlOperator(
-        task_id='insert_data',
+    create_table = MySqlOperator(
+        task_id='create_table',
         mysql_conn_id=connection_name,
         sql="""
-        SELECT * FROM olympic_dataset.athlete_bio LIMIT 1;
+        CREATE TABLE IF NOT EXISTS oleksiy.games (
+        `edition` text,
+        `edition_id` int DEFAULT NULL,
+        `edition_url` text,
+        `year` int DEFAULT NULL,
+        `city` text,
+        `country_flag_url` text,
+        `country_noc` text,
+        `start_date` text,
+        `end_date` text,
+        `competition_date` text,
+        `isHeld` text
+        );
         """
     )
 
     # Сенсор для перевірки наявності даних у таблиці
     check_for_data = SqlSensor(
-        task_id='check_for_data',
+        task_id='check_if_couns_same',
         conn_id=connection_name,
-        sql="SELECT COUNT(1) FROM olympic_dataset.athlete_bio;",
+        sql="""WITH count_in_copy AS (
+                select COUNT(*) nrows_copy from oleksiy.games
+                ),
+                count_in_original AS (
+                select COUNT(*) nrows_original from olympic_dataset.games
+                )
+               SELECT nrows_copy = nrows_original FROM count_in_copy
+               CROSS JOIN count_in_original
+               ;""",
         mode='poke',  # Режим очікування (poke або reschedule)
-        timeout=300  # Час очікування в секундах
+        poke_interval=10,  # Check every 60 seconds
+        timeout=11,  # Timeout after 10 minutes
     )
 
     # Завдання для вибору даних з таблиці
-    select_data = MySqlOperator(
-        task_id='select_data',
-        mysql_conn_id='my_mysql_conn_id',
-        sql="SELECT * FROM olympic_dataset.athlete_bio LIMIT 1;",
+    refresh_data = MySqlOperator(
+        task_id='refresh',
+        mysql_conn_id=connection_name,
+        sql="""
+            TRUNCATE oleksiy.games;
+            INSERT INTO oleksiy.games SELECT * FROM olympic_dataset.games;
+        """,
     )
 
     # Встановлення залежностей
-    create_table >> insert_data >> check_for_data >> select_data
+    create_schema >> create_table >> check_for_data >> refresh_data
