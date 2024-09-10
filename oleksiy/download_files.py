b131eac2d3d1d864aa82de81f95b02dd8383ecb6
OleksiyF <38247546+OleksiyF@users.noreply.github.com>
2024-09-10 22:00:42 +0300

    Update download_files.py

--- oleksiy/download_files.py
+++ oleksiy/download_files.py
@@ -13,19 +13,18 @@ default_args = {
     'retry_delay': timedelta(minutes=1),
 }
 
-# Define the DAG
-dag = DAG(
-    'download_games.csv',
-    default_args=default_args,
-    schedule_interval=timedelta(days=1),
-    start_date=datetime(2024, 1, 1),
-    catchup=False,
-    tags=['oleksiy']
-)
+with DAG(
+        'download_games',
+        default_args=default_args,
+        schedule_interval=None,  # DAG не має запланованого інтервалу виконання
+        catchup=False,  # Вимкнути запуск пропущених задач
+        tags=["oleksiy"]  # Теги для класифікації DAG
+) as dag:
 
 list_files_1 = BashOperator(
         task_id='list_files',  # Unique identifier for the task
-        bash_command='ls -l'   # Bash command to execute
+        bash_command='ls -l',   # Bash command to execute
+        dag=dag
     )
 
 download_ftp_file = FTPFileTransmitOperator(
@@ -34,11 +33,13 @@ download_ftp_file = FTPFileTransmitOperator(
         local_filepath='neoversity/olympics_dataset/games.csv',  # Replace with the local destination path
         remote_filepath='.',  # Replace with the path to the file on the FTP server
         operation='get',  # 'get' operation to download the file
+        dag=dag
     )
 
 list_files_2 = BashOperator(
         task_id='list_files',  # Unique identifier for the task
-        bash_command='ls -l'   # Bash command to execute
+        bash_command='ls -l',   # Bash command to execute
+        dag=dag
     )
 
 # Set task dependencies (if any)
