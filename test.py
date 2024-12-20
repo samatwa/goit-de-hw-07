c5a2e213b433c3b562c79ceeb2ce130ad19a1a3a
Eugene Churylov <75759692+EugeneChurylov@users.noreply.github.com>
2024-12-20 20:19:04 +0100

    Шифрування файлів та додавання до репозиторію_6_churylov.

--- test.py
+++ test.py
@@ -90,9 +90,6 @@ with DAG(
         """,
     )
 
-    # Завдання-заглушка для об'єднання
-    join_task = EmptyOperator(task_id='join_task')
-
     # 4. Завдання для затримки
     generate_delay_task = PythonOperator(
         task_id='generate_delay',
@@ -117,6 +114,6 @@ with DAG(
 
     # Зв’язки між задачами
     create_table >> pick_medal_task
+    create_table >> generate_delay_task  # Виконання затримки завжди
     pick_medal_task >> [calc_Bronze, calc_Silver, calc_Gold]
-    [calc_Bronze, calc_Silver, calc_Gold] >> join_task >> generate_delay_task
     generate_delay_task >> check_for_correctness
