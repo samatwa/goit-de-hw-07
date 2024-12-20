b75293c9774bd80be0139feb0cbb4edad7eba8f5
Eugene Churylov <75759692+EugeneChurylov@users.noreply.github.com>
2024-12-20 20:02:58 +0100

    Шифрування файлів та додавання до репозиторію_5_churylov.

--- test.py
+++ test.py
@@ -112,8 +112,11 @@ with DAG(
         mode='poke',
     )
 
+    # Завдання-заглушка для об'єднання
+    join_task = EmptyOperator(task_id='join_task')
+
     # Зв’язки між задачами
     create_table >> pick_medal_task
     pick_medal_task >> [calc_Bronze, calc_Silver, calc_Gold]
-    [calc_Bronze, calc_Silver, calc_Gold] >> generate_delay_task
+    [calc_Bronze, calc_Silver, calc_Gold] >> join_task >> generate_delay_task
     generate_delay_task >> check_for_correctness
