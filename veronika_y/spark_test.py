b5ba900d40f89780b4a5decace9bc26747d9b445
NikaYakymchuk <nika1881nika@gmail.com>
2025-01-27 20:52:36 +0200

    test4

--- veronika_y/spark_test.py
+++ veronika_y/spark_test.py
@@ -4,12 +4,12 @@ from pyspark.sql.types import StructType, StructField, IntegerType
 spark = SparkSession.builder \
     .appName("JDBCToKafka") \
     .getOrCreate()
-
-schema = StructType(
-    [StructField("tttt", IntegerType(), True)
-    ])
-
-df = spark.createDataFrame([(1)], schema=schema)
-df.show()
+print("hello")
+# schema = StructType(
+#     [StructField("tttt", IntegerType(), True)
+#     ])
+#
+# df = spark.createDataFrame([(1)], schema=schema)
+# df.show()
 
 
