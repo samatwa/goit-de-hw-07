3708aff223ea5a60c008b6b4ed3043562c0c4838
yevheniihorbatyuk <eugenesazak96@gmail.com>
2025-02-04 13:39:53 +0200

    test 5

--- test_dag.py
+++ test_dag.py
@@ -12,7 +12,7 @@ default_args = {
 }
 
 with DAG(
-    'test_dummy_dag_4',
+    'test_dummy_dag_5',
     default_args=default_args,
     description='Тестовий DAG, який нічого не робить',
     schedule_interval=None,  # Виконання тільки вручну
